438406bc34a24795b541c3d9044c0f6022b6ff33
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-03-10 12:36:45 +0000

    First commit

---
+++ ArrayBiDim/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrayBiDim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float somadamedia = 0;
+            float soma = 0;
+            Console.WriteLine("Gibe horizontal");
+            int x = Console.ReadLine();
+            Console.WriteLine("Gibe vertical");
+            int y = Console.ReadLine();
+
+            float [,] array = new float [x,y];
+            
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.WriteLine("Gibe num");
+                    float num = Console.ReadLine();
+                    array[i,j] = num;
+                    soma += num;
+                }
+                somadamedia += soma/array.GetLength(1)
+                Console.WriteLine($"Media da linha e {soma/array.GetLength(1)}");
+                soma = 0
+            }
+
+
+        }
+    }
+}
---
+++ PercorreString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NomeDoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gibe string");
+            string old = Console.ReadLine();
+            Console.WriteLine("Gibe caracter");
+            char remove = Console.ReadLine();
+            
+            for (int i = 0; i < old.Length; i++)
+            {
+                if (old[i] == remove)
+                {
+                
+                }
+                else
+                {
+                Console.Write($"{old[i]}");
+                }
+            }
+
+        }
+    }
+}
