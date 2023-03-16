1560042c46d94b0e648a375605a7e388aa3740d8
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-16 21:39:21 +0000

    Base line atempt 1

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,19 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Gimme words");
+            string words = Console.ReadLine();
+            Console.WriteLine("Gimme char");
+            string toremove = Console.ReadLine();
+            char removechar = Convert.ToChar(toremove);
+
+            foreach (char c in words)
+            {
+                if (removechar == c)
+                    break;
+                else
+                    Console.Write($"{c}");
+            }
         }
     }
 }
