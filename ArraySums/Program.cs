ea0a6b9773fff4eb1a5e9b5d8a013e64f3753a84
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-16 22:25:57 +0000

    Finished array building

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -6,7 +6,30 @@ namespace ArraySums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Gimme lengh");
+            string str = Console.ReadLine();
+            int l = Convert.ToInt16(str);
+            Console.WriteLine("Gimme height");
+            str = Console.ReadLine();
+            int h = Convert.ToInt16(str);
+
+            int num;
+
+            int[,] matrix = new int[l, h];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Gimme num for place {i}, {j}");
+                    str = Console.ReadLine();
+                    num = Convert.ToInt16(str);
+
+                    matrix[i, j] = num;
+
+                }
+            }
+
         }
     }
 }
