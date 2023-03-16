228e1449d210245f1102c827e68977d439e5f78b
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-16 23:28:52 +0000

    Add Enum

---
+++ PlayerPerks/Perks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Perks
+{
+    [Flags]
+    enum WoWRoles
+    {
+        Tank = 1 << 0, // 1
+        Healer = 1 << 1, // 2
+        Damage = 1 << 2 // 4
+    };
+
+}
