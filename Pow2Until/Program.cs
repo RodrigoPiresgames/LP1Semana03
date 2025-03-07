6935e0080d37eece8b67e2090beab8341d44111b
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-03-07 13:51:09 +0000

    Add Pow

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pow2Until
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+
+        private static void PowerOf2Until5()
+        {
+            for (int i = 1; i <= (1<<5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
