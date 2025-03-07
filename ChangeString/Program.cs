bc2e32bac7876177d702ec5ec29ecae26ca428d8
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-03-07 13:15:59 +0000

    ChangeString

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,19 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Gimme words");
+            string words = Console.ReadLine();
+            Console.WriteLine("Gimme char");
+            string toremove = Console.ReadLine();
+            char removechar = Convert.ToChar(toremove);
+
+            foreach (char c in words)
+            {
+                if (removechar == c)
+                    continue;
+                else
+                    Console.Write($"{c}");
+            }
         }
     }
 }
